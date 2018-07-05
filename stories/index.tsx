5525d2984382e1f27f419cb19425608759553404
i78s <inaba182@gmail.com>
2018-07-06 00:00:29 +0900

    Introduce storybook

---
+++ stories/index.tsx
@@ -0,0 +1,10 @@
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import App from '../src/App';
+const Sample = () => <div>sample</div>;
+
+storiesOf('app', module).add('to Storybook', () => <App />);
+storiesOf('Welcome', module).add('to Storybook', () => <Sample />);
