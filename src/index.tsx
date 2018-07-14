b3761412613f5625e90f987b2e3488f3f18ff0d8
i78s <inaba182@gmail.com>
2018-07-14 11:18:11 +0900

    Add polyfills.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import App from 'App';
+import 'polyfills';
 import registerServiceWorker from 'registerServiceWorker';
 
 import 'index.css';
---
+++ src/polyfills.ts
@@ -0,0 +1,2 @@
+// See https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#supported-language-features-and-polyfills
+// Only includes a object-assign, promise, whatwg-fetch
